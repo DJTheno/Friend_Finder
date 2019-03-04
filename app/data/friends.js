eedb3c23c73fe819201ad2e04b0a568e246eeab2
Dylan Theno <djtheno@gmail.com>
2019-03-03 20:09:10 -0600

    bugged

---
+++ app/data/friends.js
@@ -0,0 +1,195 @@
+var yourFriends = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"Bruce",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            3,
+            2,
+            5,
+            3,
+            2,
+            2,
+            5,
+            2
+          ]
+      },
+
+      {
+        "name":"Clark",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            4,
+            1,
+            4,
+            3,
+            4,
+            4,
+            2,
+            4,
+            3,
+            2
+          ]
+      },
+
+      {
+        "name":"Hal",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            2,
+            3,
+            3,
+            4,
+            2,
+            2,
+            2,
+            5,
+            1
+          ]
+      },
+
+      {
+        "name":"Diana",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            4,
+            2,
+            5,
+            5,
+            5,
+            2,
+            2,
+            1,
+            1,
+            1
+          ]
+      },
+
+      {
+        "name":"Arthor",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            3,
+            3,
+            4,
+            3,
+            2,
+            4,
+            5,
+            3,
+            1
+          ]
+      },
+      
+      {
+        "name":"Oswald",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            4,
+            2,
+            5,
+            5,
+            5,
+            3,
+            4,
+            2,
+            5,
+            3
+          ]
+      },
+      {
+        "name":"ED",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            4,
+            3,
+            4,
+            2,
+            5,
+            4,
+            1,
+            3,
+            5
+          ]
+      },
+
+      {
+        "name":"Joker",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+          ]
+      },
+      {
+        "name":"Harley",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+          ]
+      },
+      
+      {
+        "name":"Alfred",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            4,
+            3,
+            4,
+            4,
+            3,
+            3,
+            4,
+            5,
+            3
+          ]
+      },
+      
+      
+      
+      
+      
+      
+
+
+      
+]
